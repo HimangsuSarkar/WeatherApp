a9970f417adaec399bd32a3c61bd582b3f0a7194
HimangsuSarkar <himangsusarkar1@gmail.com>
2021-06-03 16:35:25 +0800

    added location in app

--- App.js
+++ App.js
@@ -3,6 +3,10 @@ import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Location from 'expo-location';
 
+const  WEATHER_API_KEY ='a5d943ebd115e365117562df62a57f98';
+
+const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?'
+
 export default function App() {
 
   const [errorMessage, setErrorMessage] = useState(null)
@@ -19,6 +23,12 @@ async function load(){
       setErrorMessage('Access to location is needed to run the app')
       return
   }
+  const location = await Location.getCurrentPositionAsync()
+  const { latitude, longitude } = location.coords
+  const weatherUrl = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${WEATHER_API_KEY}`
+
+ 
+
 
   }catch(error){}
 }
